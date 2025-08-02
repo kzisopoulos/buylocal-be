283fb5ba3cb180ec29ce88932c13f41103372c50
Konstantinos Zisopoulos <k91.zis@gmail.com>
2025-08-02 13:05:49 +0300

    feat: added routes and controllers for auth endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import { Listing, PrismaClient } from "@prisma/client";
 import cors from "cors";
 import corsOptions from "./config/cors";
+import authRoutes from "./routes/auth.routes";
 
 const prisma = new PrismaClient();
 const app: Express = express();
@@ -9,10 +10,7 @@ const app: Express = express();
 app.use(cors(corsOptions));
 const port = process.env.PORT || 3000;
 
-app.get("/", async (req: Request, res: Response) => {
-  const listings: Listing[] = await prisma.listing.findMany();
-  res.send(listings.map((l) => `<div>${l.name}</div>`).join(""));
-});
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
